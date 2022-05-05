9246f5f40479267de1c74e6b066b0a86b20bef92
devilliers.neethling <DeVilliers.Neethling@standardbank.co.za>
2022-05-05 09:59:53 +0200

    Searching for movies with the api yay

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import MovieList from './components/MovieList';
 import { Constants } from './Constants';
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading'
+import SearchBox from './components/SearchBox'
+
 
 
 const App = () => {
 	const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
 
-  const getMovieRequest = async ()  => {
-    const url = `http://www.omdbapi.com/?s=star trek&apikey=${Constants.OMDB_API_KEY}`;
+  const getMovieRequest = async (searchValue)  => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=${Constants.OMDB_API_KEY}`;
 
     const response = await fetch(url);
     const responseJson = await response.json();
@@ -21,10 +25,17 @@ const App = () => {
 
   useEffect(() =>{
     getMovieRequest();
-  }, []);
+  }, [searchValue]);
 
 	return (
 		<div className='container-fluid movie-app'>
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <MovieListHeading heading='Movies'  />
+        <SearchBox 
+          searchValue={searchValue} 
+          setSearchValue={searchValue} 
+          />
+      </div>
 			<div className='row'>
 				<MovieList movies={movies} />
 			</div>
