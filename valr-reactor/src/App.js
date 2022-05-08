883645e491b1249f52b1bbd5adc7fc581146b6ba
devilliers.neethling <DeVilliers.Neethling@standardbank.co.za>
2022-05-08 17:13:32 +0200

    Tooling with material ui. Got a container and some typography

--- valr-reactor/src/App.js
+++ valr-reactor/src/App.js
@@ -1,7 +1,24 @@
+import { Container, Typography } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 
 function App() {
-  return <div></div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://finalspaceapi.com/api/v0/character/?limit=12")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  });
+  
+  return (
+  <div>
+    <Container>
+      <Typography color="textPrimary" gutterBottom variant="h2" align="center">
+      Is there anybody out there? Don't feed the algorithms
+      </Typography>
+    </Container>
+  </div>
+  );
 }
 
 export default App;
